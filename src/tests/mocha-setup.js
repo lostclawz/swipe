59241fbf08147c9e84caf6f824b043cdf8e01130
Philip Kuperberg <philipkuperberg@gmail.com>
2018-11-11 18:25:35 -0500

    updated mocha setup for babel 7

--- src/tests/mocha-setup.js
+++ src/tests/mocha-setup.js
@@ -1,12 +1,4 @@
-require('babel-core/register')({
-	presets: [ "env", "react" ],
-	plugins: [
-		"transform-class-properties",
-      	"transform-decorators-legacy",
-      	"transform-object-rest-spread",
-      	"transform-es2015-destructuring"
-  	]
-});
+require('@babel/register');
 var configure = require('enzyme').configure;
 var Adapter = require('enzyme-adapter-react-16');
 configure({ adapter: new Adapter() });
